c5ac84bd9affb93b3e6f94b305df3298a2f36d5a
Sophia Speredelozzi <Spere.Soph2018@ssusd.org>
2018-02-05 07:40:43 -0800

    Opoerators work

---
+++ Operators.java
@@ -0,0 +1,25 @@
+class Operators {
+    public static void main(String args[]) {
+        // String name = "Speredelozzi";
+        // System.out.println(name);
+
+    //     String firstName = "Sophia";
+    // String lastName = "Speredelozzi";
+    // String fullName = firstName + " " + lastName;
+
+    // System.out.println(firstName);
+    // System.out.println("firstName");
+    // System.out.println(lastName);
+    // System.out.println("lastName");
+    // System.out.println(fullName);
+    // System.out.println("fullName");
+
+     int currentYear = 2018;
+    int yearsUntilGraduation = 0;
+    int myGradYear = currentYear + yearsUntilGraduation;
+
+    System.out.println(myGradYear);
+    System.out.println("myGradYear");
+
+    }
+}
