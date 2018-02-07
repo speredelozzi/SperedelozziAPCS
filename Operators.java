865618804f04cfbf9deebe98fb2797269f8c9181
speredelozzi <sophia.speredelozzi@gmail.com>
2018-02-06 21:30:32 -0800

    operators work

--- Operators.java
+++ Operators.java
@@ -14,12 +14,105 @@ class Operators {
     // System.out.println(fullName);
     // System.out.println("fullName");
 
-     int currentYear = 2018;
-    int yearsUntilGraduation = 0;
-    int myGradYear = currentYear + yearsUntilGraduation;
+    //  int currentYear = 2018;
+    // int yearsUntilGraduation = 0;
+    // int myGradYear = currentYear + yearsUntilGraduation;
 
-    System.out.println(myGradYear);
-    System.out.println("myGradYear");
+    // System.out.println(myGradYear);
+    // System.out.println("myGradYear");
+
+     //Replace the -1's with values relevant to yourself
+    // int currentAge = 17;
+    // int nextAge = currentAge + 1;
+    // System.out.println(currentAge);
+    // System.out.println(nextAge);
+
+    // int halfCurrentAge = currentAge / 2;
+    // int halfNextAge = nextAge / 2;
+    // System.out.println(halfCurrentAge);
+    // System.out.println(halfNextAge);
+
+    // //"Modulo" is the name of the '%' operator. Can you figure out what the modulo operator is doing?
+    // int moduloCurrentAge = currentAge % 2;
+    // int moduloNextAge = nextAge % 2;
+    // System.out.println(moduloCurrentAge);
+    // System.out.println(moduloNextAge);
+
+//  double bitcoinsOwned = .000001;
+//     double bcsCoinsOwned = 2;
+//     double bitcoinValue = 10000;
+//     double bcsCoinValue = 0;
+//     double bitcoinsWorth = bitcoinsOwned * bitcoinValue;
+//     double bcsCoinsWorth = bcsCoinsOwned * bcsCoinValue;
+//     System.out.println("My current bitcoins are worth $" + bitcoinsWorth);
+//     System.out.println("My current BCSCoins are worth $" + bcsCoinsWorth);
+//     //Let's assume I can get it so you can redeem 25 BCSCoins for a $5 giftcard.
+//     double reward = 5;
+//     double amtToRedeem = 25;
+//     bcsCoinValue = reward / amtToRedeem; //Reassign bcsCoinValue to the potential
+//     System.out.println("One BCSCoin might be worth $" + bcsCoinValue + " later. If so...");
+//     //This is the exact same calculation we used earlier, but we updated bcsCoinValue, so let's calculate it again.
+//     bcsCoinsWorth = bcsCoinsOwned * bcsCoinValue; 
+//     System.out.println("My current BCSCoins could eventually be worth $" + bcsCoinsWorth);    
+
+//  Boolean haveJob = false;
+//     Boolean lookingForWork = true;
+//     Boolean canProgram = false;
+//     int currentSavings = 500;
+
+//     //There are some print statements below. Can you guess which ones will print?
+//     if(!haveJob) {
+//         if(lookingForWork && canProgram) {
+//             System.out.println("Your chances as getting a job go up!");
+//         }
+//         else {
+//             System.out.println("You might need to gain more skills before you can get a job.");
+//         }
+//     }
+//     if(currentSavings < 500) {
+//         System.out.println("You should be saving more money.");
+//     }
+//     else {
+//         System.out.println("Do you need to save more money, still?");
+//     }
+
+// //savings code
+// int targetSavingsGoal = 8400;
+// int savingsPerMonth = 80;
+// double monthsToGoal = targetSavingsGoal / savingsPerMonth;
+// double yearsToGoal = monthsToGoal / 12;
+// System.out.println(monthsToGoal);
+// System.out.println(yearsToGoal);
+
+// //savings code part 2
+// int targetSavingsGoal = 8400;
+// int timeGoalReach = 12;
+// double savingsPerMonth = targetSavingsGoal / timeGoalReach;
+// System.out.println(savingsPerMonth);
+
+// // sentences code
+// String verb1 = "screeched";
+// String verb2 = "shimmied";
+// String name1 = "Trevor";
+// String name2 = "Frank";
+// String name3 = "Ryan";
+// String prep1 = "into";
+// String prep2 = "around";
+// String adv1 = "enthusiastically";
+// String adv2 = "flamboyantly";
+// String adv3 = "together";
+// String place1 = "sunset";
+// String place2 = "void";
+// String article1 = "the";
+// String conj1 = "and";
+
+// String sentence1 = name2 + " " + verb1 + " " + adv2 + " " + prep1 + " " + article1 + " " + place2;
+// String sentence2 = name3 + " " + verb2 + " " + adv1 + " " + prep2 + " " + article1 + " " + place2;
+// String sentence3 = name1 + " " + conj1 + " " + name2 + " " + conj1 + " " + name3 +  " " + verb1 + " " + conj1 + " " + verb2 + " " + prep1 + " " + article1 + " " + place1 + " " + adv3;
+// System.out.println(sentence1);
+// System.out.println(sentence2);
+// System.out.println(sentence3);
+
 
     }
 }
