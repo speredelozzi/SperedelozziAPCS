c86b6af4a3c6a575f869959acfa26c061b883e17
Sophia Speredelozzi <Spere.Soph2018@ssusd.org>
2018-02-23 07:39:02 -0800

    while loops finish

--- Assignments/Loops/whileloops.java
+++ Assignments/Loops/whileloops.java
@@ -28,13 +28,51 @@ class whileLoops {
         //     n++;
         // } // prediction: 0,1,2,3...19
 
-        int n = 0;
-        while(true) {
-            System.out.println("Hello World");
-            n++;
-            if(n == 10){
+        // int n = 0;
+        // while(true) {
+        //     System.out.println("Hello World");
+        //     n++;
+        //     if(n == 10){
+        //         break;
+        //     }
+        // } //prediction: "Hello World" prints 10 times
+
+        // int n = 1;
+        // while(n < 10) {
+        //     System.out.println(n);
+        //     n++;
+        // }
+
+        // Boolean shouldLoop = true;
+        //     int n = 1;
+        //     while(shouldLoop) {
+        //         if(n == 100) {
+        //             System.out.println("100 loop passes occured.");
+        //             shouldLoop = false;
+        //         }
+        //         n++;
+        //     }
+
+        // int n = 100;
+        // while (n >= -100) {
+        //     System.out.println(n);
+        //     n--;
+        // }
+
+        // int n = 0;
+        // while (n <= 120) {
+        //     System.out.println(n);
+        //     n += 3;
+        // }
+
+        char alpha = 'a';
+        while (alpha <= 'z') {
+            System.out.println(alpha);
+            alpha++;
+            if (alpha == 'z') {
+                System.out.println(alpha);
                 break;
             }
-        } //prediction: "Hello World" prints 10 times
+        }
     }
 }
