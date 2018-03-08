8b6fac493460041bb52d32f9a51eb599024c1055
Sophia Speredelozzi <Spere.Soph2018@ssusd.org>
2018-03-08 07:39:22 -0800

    scopes

---
+++ Assignments/Scope/ScopeTest.java
@@ -0,0 +1,38 @@
+class ScopeTest {
+        public static String message = "Hello";
+        public static Boolean shouldLoop = false;
+
+        public static void main(String args[]) {
+            // int i;
+            // while(i <= 10) {
+            //     int n = i * 2;
+            //     System.out.println(n);
+            //     i++;
+
+            //     if(n > 10) {
+            //         String message = "Hello World";
+            //         System.out.println(message);
+            //     }
+            // }
+
+            // Boolean shouldLoop = true;
+            // i = 0;
+            // while(shouldLoop) {
+            //     if(i < 10) {
+            //         System.out.println(message);                    
+            //     } else {
+            //         shouldLoop = false;
+            //     }
+            //     i++;
+            // }
+            // String message = "Hello World";
+            // for(int i = 0; i < message.length(); i++) {
+            //     if(message.charAt(i) == 'W') {
+            //         break;
+            //     }
+            // }
+            // System.out.println("W found at index " + i);
+            // }
+        }
+}
+        
