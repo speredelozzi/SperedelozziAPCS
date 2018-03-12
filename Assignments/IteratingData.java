aa4703bd815254834b970db3322a62a66e6c72da
Sophia Speredelozzi <Spere.Soph2018@ssusd.org>
2018-03-12 07:38:56 -0700

    iterating data

---
+++ Assignments/IteratingData.java
@@ -0,0 +1,10 @@
+class IteratingData {
+
+    public static void main(String args[]) {
+        int[] nums = new int[201];
+        int i = -100;
+        for(int value : nums) {
+            System.out.println(value);
+        }
+    }
+}
