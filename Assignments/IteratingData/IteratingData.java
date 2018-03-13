9b13d50f533a71e4f7ec50ddfae8ad8e06fb297d
Sophia Speredelozzi <Spere.Soph2018@ssusd.org>
2018-03-13 07:39:36 -0700

    iterating data

--- Assignments/IteratingData/IteratingData.java
+++ Assignments/IteratingData/IteratingData.java
@@ -1,9 +1,22 @@
 class IteratingData {
 
     public static void main(String args[]) {
-        int[] nums = new int[201];
-        nums[0] = -100;
-        for(int i = 0; i < nums.length; i++) {
+        // int[] nums = new int[202];
+
+        // nums[0] = -101;
+
+        // for(int i = 1; i < nums.length; i++) {
+        //     nums[i] = nums[i - 1] + 1;
+        //     System.out.println(nums[i]);
+        // }
+
+        int[] nums = new int[202];
+
+        nums[0] = -101;
+
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] + 1;
+            java.lang.Math.abs(nums[i]);
             System.out.println(nums[i]);
         }
     }
