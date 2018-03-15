579abbdb9bb972b0b748bcdac61930b0f64328d6
speredelozzi <sophianinjastar@icloud.com>
2018-03-14 21:16:42 -0700

    scopes finish

--- Assignments/Scope/ScopeTest.java
+++ Assignments/Scope/ScopeTest.java
@@ -1,19 +1,19 @@
- class ScopeTest {
+class ScopeTest {
+        // public static String message = "Hello";
+        // public static Boolean shouldLoop = false;
+        // public static int i = -1;
+        // public static int n = -1;
 
-        public static String message = "Hello";
-        public static Boolean shouldLoop = false;
-        public static int i;
-
-        public static void main(String args[]) {
+        // public static void main(String args[]) {
             // while(i <= 10) {
-            //     int n = i * 2;
+            //     n = i * 2;
             //     System.out.println(n);
             //     i++;
+            // }
 
-            //     if(n > 10) {
-            //         String message = "Hello World";
-            //         System.out.println(message);
-            //     }
+            // if(n > 10) {
+            //     String message = "Hello World";
+            //     System.out.println(message);
             // }
 
             // Boolean shouldLoop = true;
@@ -26,14 +26,32 @@
             //     }
             //     i++;
             // }
-            String message = "Hello World";
-            for(int i = 0; i < message.length(); i++) {
-                if(message.charAt(i) == 'W') {
-                    break;
-                }
-            }
-            int i = 6;
-            System.out.println("W found at index " + i);
+
+        // String message = "Hello World";
+        // int i;
+        // for(i = 0; i < message.length(); i++) {
+        //     if(message.charAt(i) == 'W') {
+        //         break;
+        //     }
+        // }
+        // System.out.println("W found at index " + i);
+        // }
+
+// class Powers {
+    
+    public static final int n = 32;
+    public static int base = 2;
+
+    public static void main(String[] args) {
+        for(int i = 0; i <= n; i++) {
+            power(i);            
         }
     }
-        
+
+    public static double power(int i) {
+        double x = Math.pow( base , i );
+        System.out.println(x);
+        return x;
+    }
+}
+    
