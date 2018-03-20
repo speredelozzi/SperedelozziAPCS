072741fe21b1a65d1dea30ea80617360107cd1c8
speredelozzi <sophia.speredelozzi@gmail.com>
2018-03-19 21:22:09 -0700

    lists frame

--- Assignments/Lists/Lists.java
+++ Assignments/Lists/Lists.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 class Lists {
 
+    public static int add = 1;
+    public static int remove = 2;
+    public static int view = 3;
+    public static int mode = 0;
+    public static int quit = 4;
+    public static String task;
+
     public static void main(String args[]) {
 
         // ArrayList<Integer> numList = new ArrayList<Integer>();
@@ -22,19 +29,83 @@ class Lists {
         // //OG prediction: {5,10,15,20,1,25,0,30,3,35}
         // //Second ediition prediction: 30,10,35,5,20
 
-        ArrayList<Integer> toDo = new ArrayList<Integer>();
-
-        toDo.add(1);
-        toDo.add(2);
-        toDo.add(3);
         
-        for(int i = 0; i < toDo.size(); i++) {
-            System.out.println("To Do List: [" + i + "]" + toDo.get(i));
-            // Scanner scan;
-            // scan = new Scanner(System.in);
-            // if(nextLine()) {
+        
+        Scanner scan;
+        while(true) {
+            scan = new Scanner(System.in);
+            modeStatement();
+            if(scan.hasNextInt()) {
+                mode = scan.nextInt();
+                if(mode == add) {
+                    addTask();
+                }
+                else if(mode == remove) {
+                    removeTask();
+                }
+                else if(mode == view) {
+                    viewList();
+                }
+                else if(mode == quit) {
+                    break;
+                }
+                else {
+                    break;
+                }
+            }
+        }        
+        // for(int i = 0; i < toDo.size(); i++) {
+        //     String task;
+        //     Scanner scan;
+        //     while(true) {
+        //         scan = new Scanner(System.in);
+        //         System.out.println(i + " : ");
+        //         if(scan.hasNextLine()) {
+        //             task = scan.nextLine();
+        //             System.out.println("To Do List: [" + i + "]" + task);
+        //         }    
+        //     }
+        // }
+    }
 
-            // }
+    public static void modeStatement() {
+        System.out.println("\n"
+            + "Options:\n"
+            + "(1) add a task \n"
+            + "(2) remove a task \n"
+            + "(3) view the list \n"
+            + "(4) quit" );
+    }
+
+    public static void addTask() {
+        Scanner scan;
+        while(true) {
+            scan = new Scanner(System.in);
+            System.out.print("New task: ");
+            if(scan.hasNextLine()) {
+                task = scan.nextLine();
+                break;
+            }
+        }           
+    }
+
+    public static void removeTask() {
+        Scanner scan;
+        while(true) {
+            scan = new Scanner(System.in);
+            System.out.print("Remove task: ");
+            if(scan.hasNextLine()) {
+                task = scan.nextLine();
+                break;
+            }
+        }        
+    }
+
+    public static void viewList() {
+        ArrayList<Integer> toDo = new ArrayList<Integer>();
+        toDo.add(0);
+        for(int i = 0; i < toDo.size(); i++) {
+            System.out.println("To Do List: [" + i + "] " + toDo.get(i));
         }
     }
 }
